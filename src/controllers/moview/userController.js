b037c367c2939b9d8a926eb615d87279a59af1c3
Ajay Spak <ajayspakcommcom>
2025-01-02 11:44:26 +0530

    changes

--- src/controllers/moview/userController.js
+++ src/controllers/moview/userController.js
@@ -1,6 +1,17 @@
 const bcrypt = require('bcrypt');
 const User = require('../../models/moview/userModel');
 
+const Follower = require('../../models/moview/followerModel');
+const Following = require('../../models/moview/followingModel');
+const Favorites = require('../../models/moview/favouriteModel');
+const Notification = require('../../models/moview/notificationModel');
+const Review = require('../../models/moview/reviewModel');
+const ReviewShow = require('../../models/moview/reviewShowModel');
+const ShowNotification = require('../../models/moview/showNotificationModel');
+
+
+
+
 exports.getAllUsers = async (req, res) => {
     try {
         const users = await User.find({});
@@ -82,13 +93,36 @@ exports.updateUserById = async (req, res) => {
     }
 };
 
+// exports.deleteUserById = async (req, res) => {    
+//     try {
+//         const user = await User.findOneAndUpdate({ _id: req.params.id }, { $set: { is_deleted: true } }, { new: true });
+//         if (!user) {
+//             return res.status(404).json({ status: 'fail', message: 'No user found with that ID' });
+//         }
+//         return res.status(200).json({ status: 'success', data: null, message: 'User deleted successfully' });
+//     } catch (error) {
+//         return res.status(500).json({ status: 'error', message: 'Server error: Cannot delete the user.' });
+//     }
+// };
+
 exports.deleteUserById = async (req, res) => {    
-    try {
-        const user = await User.findOneAndUpdate({ _id: req.params.id }, { $set: { is_deleted: true } }, { new: true });
+    try {        
+        const user = await User.findByIdAndDelete(req.params.id);
         if (!user) {
+
+            await Promise.all([
+                Follower.deleteMany({ userId: req.params.id }),
+                Following.deleteMany({ userId: req.params.id }),
+                Favorites.deleteMany({ user: req.params.id }),
+                Notification.deleteMany({ user_id: req.params.id }),
+                Review.deleteMany({ user: req.params.id }),
+                ReviewShow.deleteMany({ user: req.params.id }),
+                ShowNotification.deleteMany({ user_id: req.params.id })
+            ]);
+
             return res.status(404).json({ status: 'fail', message: 'No user found with that ID' });
         }
-        return res.status(200).json({ status: 'success', data: null, message: 'User deleted successfully' });
+        return res.status(200).json({ status: 'success', data: null, message: 'User deleted permanently' });
     } catch (error) {
         return res.status(500).json({ status: 'error', message: 'Server error: Cannot delete the user.' });
     }
